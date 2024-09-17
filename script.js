01f2c95dbc2a50e6055b6ccfcbe4d207a23dbd0b
RachnaRajinder <rachnasoni548@gmail.com>
2024-09-17 18:16:00 +0300

    make_changes

--- script.js
+++ script.js
@@ -4,19 +4,7 @@ const body = document.querySelector("body");
 console.log(body);
 btn.forEach(function (button) {
   button.addEventListener("click", function (e) {
-    console.log(e);
-
-    if (e.target.id == "color_1") {
-      body.style.backgroundColor = "#FF00FF";
-    }
-    else if (e.target.id == "color_2") {
-      body.style.backgroundColor = "#00FFFF";
-    }
-    else if (e.target.id == "color_3") {
-      body.style.backgroundColor = "#000000";
-    }
-    else if (e.target.id == "color_4") {
-      body.style.backgroundColor = "#2f0be3";
-    }
+    const color = e.target.style.backgroundColor;
+    body.style.backgroundColor = color;
   });
 });
